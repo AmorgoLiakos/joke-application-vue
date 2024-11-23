661dc94698b4a759f721b74928fed89c069cd5c7
Liakos <liakos.prekas@gmail.com>
2024-11-23 18:33:27 +0200

    create all jokes template structure

---
+++ src/components/Jokes.vue
@@ -0,0 +1,20 @@
+<script setup>
+import JokeCard from '@/components/JokeCard.vue'
+</script>
+
+<template>
+  <div class="jokes">
+    <JokeCard v-for="i in (12)" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .jokes {
+    padding: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+</style>
