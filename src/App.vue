06fcf93701fc14b34cc836dc38be9e5589eae236
Liakos <liakos.prekas@gmail.com>
2024-11-23 18:33:47 +0200

    create homepage structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
+import Jokes from "@/components/Jokes.vue";
 </script>
 
 <template>
-<h1>Jokes App</h1>
+  <div class="homepage">
+    <h1 class="homepage__title">Jokes Application</h1>
+    <Jokes />
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+  .homepage {
+    &__title {
+      text-align: center;
+      color: #2c3e50;
+    }
+  }
 </style>
