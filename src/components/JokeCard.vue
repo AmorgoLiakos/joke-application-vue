c64be22c954a80ec4e2650d4da65d40c32d16367
Liakos <liakos.prekas@gmail.com>
2024-11-23 18:23:35 +0200

    create single joke card template structure

---
+++ src/components/JokeCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="single-joke-card">
+    <div class="single-joke-card__head">
+      <span>add to my collection btn goes here</span>
+    </div>
+    <div class="single-joke-card__body">
+      <span class="single-joke-card__setup">setup goes here</span>
+      <span class="single-joke-card__punchline">punchline goes here</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .single-joke-card {
+    padding: 2rem;
+    border: 1px solid #000;
+    border-radius: 0.5rem;
+    &__head {
+      padding: 1rem;
+      border-radius: 0.3rem;
+      background-color: lightseagreen;
+      margin-bottom: 0.5rem;
+    }
+    &__body {
+      padding: 1rem;
+      border-radius: 0.3rem;
+      background-color: lightgray;
+      display: flex;
+      flex-direction: column;
+    }
+  }
+</style>
