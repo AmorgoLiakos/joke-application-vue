504833f5e92b15c2a32854b775ebdf0545af6b17
Liakos <liakos.prekas@gmail.com>
2024-11-25 01:03:52 +0200

    intro collection component

--- src/components/Collection/Collection.vue
+++ src/components/Collection/Collection.vue
@@ -1,9 +1,26 @@
-<script setup></script>
+<script setup>
+import JokeCard from '@/components/JokeCard/JokeCard.vue'
+import {ref} from "vue";
+import {inCollectionCheck} from "@/composables/inCollectionCheck.js";
+let currentCollection = localStorage.getItem('myCollection') || '[]'
+let currentCollectionParsed = JSON.parse(currentCollection);
+
+const myCollection = ref(currentCollectionParsed)
+</script>
 
 <template>
-  Collection Page
+  <div class="jokes">
+    <JokeCard v-for="joke in myCollection" :key="joke.id" :joke="joke" :inCollection="inCollectionCheck(joke.id)" />
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+.jokes {
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
 </style>
