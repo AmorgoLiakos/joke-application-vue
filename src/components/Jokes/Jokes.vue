9a7c227a9a25fbdc81e8eafca342363016889ee4
Liakos <liakos.prekas@gmail.com>
2024-11-24 17:31:17 +0200

    all jokes component

---
+++ src/components/Jokes/Jokes.vue
@@ -0,0 +1,61 @@
+<script setup>
+import JokeCard from '@/components/JokeCard/JokeCard.vue'
+import {fetchData} from "@/composables/fetchData.js";
+import {onMounted, ref} from "vue";
+
+const jokes = ref('');
+const collectionJokes = ref('');
+const loading = ref(false);
+
+const API_URLs = {
+  'home': 'https://official-joke-api.appspot.com/jokes/ten',
+  'random': 'https://official-joke-api.appspot.com/jokes/ten',
+  'programming': 'https://official-joke-api.appspot.com/jokes/programming/ten',
+}
+
+const toggleActiveClass = (elClass, elToBeActive) => {
+  Array.from(document.querySelectorAll(elClass)).forEach((el) => {el.classList.remove('active');});
+  elToBeActive.classList.add('active');
+}
+
+const toggleFunc = async (e) => {
+  loading.value = true
+  jokes.value = await fetchData(API_URLs[e.target.value.toLowerCase()]);
+  toggleActiveClass('.toggle-btn', e.target)
+  loading.value = false
+}
+const homeData = async () => {
+  loading.value = true
+  jokes.value = await fetchData(API_URLs['home'])
+  loading.value = false
+}
+const CollectionJokesData = () => {
+  collectionJokes.value = JSON.parse(localStorage.getItem('myCollection'))
+}
+
+const inCollection = (idToCheck) => {
+  let currentCollection = localStorage.getItem('myCollection') || '[]'
+  let currentCollectionParsed = JSON.parse(currentCollection);
+  return currentCollectionParsed.some( joke => joke.id === idToCheck);
+}
+
+onMounted( () => {
+  homeData()
+  CollectionJokesData()
+})
+</script>
+
+<template>
+  <div class="jokes__type-toggle">
+    <button class="toggle-btn" value="programming" @click="toggleFunc" :disabled="loading.value">Programming</button>
+    <button class="toggle-btn active" value="random" @click="toggleFunc" :disabled="loading.value">Random</button>
+  </div>
+  <div class="jokes">
+    <JokeCard v-if="!loading" v-for="joke in jokes" :key="joke.id" :joke="joke" :inCollection="inCollection(joke.id)" />
+    <div v-else> Loading...</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @import "jokes.scss";
+</style>
