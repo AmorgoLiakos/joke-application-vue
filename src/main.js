5e979cb919ab52a963b85bd2c6efbbd2cfb8704a
Liakos <liakos.prekas@gmail.com>
2024-11-26 00:12:58 +0200

    implement pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router';
+const pinia = createPinia()
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(pinia).mount('#app');
